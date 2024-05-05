a56d263afadce1e68cf5c5171cc988842c32b90d
Ahza Mazola <ahzamazola@gmail.com>
2024-05-05 23:03:00 +0700

    modified:   problem1/main.py membuat piramida bintang

--- problem1/main.py
+++ problem1/main.py
@@ -1,6 +1,14 @@
 def play_with_asterisk(n):
-    pattern = ""
-    return pattern
+    for i in range (n):
+        for k in range (n-i):
+            print(' ',end='')
+        for j in range (i+1):
+            print('* ',end='')
+        print()
+
+# Contoh penggunaan:
+# draw_pyramid(5)
+
 
 if __name__ == '__main__':
     print(play_with_asterisk(5))
