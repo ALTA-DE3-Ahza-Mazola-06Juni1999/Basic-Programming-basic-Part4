7fe6ca44f7c3d625cc7830e18fdd4cd200507aab
Ahza Mazola <ahzamazola@gmail.com>
2024-05-11 23:19:58 +0700

    modified:   problem2/main.py

--- problem2/main.py
+++ problem2/main.py
@@ -1,6 +1,14 @@
-def draw_xyz(N):
-    pattern = ""
-    return pattern
+def draw_xyz(N): 
+    pattern = ["X","Y","Z"]
+    for i in range(N):
+        for j in range (N):
+            if (i + j) % 3 == 1:
+                print(pattern[1], end ="")
+            elif (i + j) % 3 == 2:
+                print(pattern[2], end ="")
+            elif (i + j) % 3 == 0:
+                print(pattern[0], end = "")
+        print()
 
 if __name__ == '__main__':
     print(draw_xyz(3))
@@ -9,7 +17,6 @@ if __name__ == '__main__':
     Z Y X
     Y Z X
     """
-
 
     print(draw_xyz(5))
     """
